c451fcb37c98af6cb793edc1099b529039817b47
Michał Grygierzec <michal.grygierzec@protonmail.com>
2023-11-26 14:09:59 +0100

    build from src

--- 2020/day07/install.sh
+++ 2020/day07/install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get install git build-essential castxml libgc-dev
-wget 'https://quantum-mirror.hu/mirrors/pub/gnusavannah/liberty-eiffel/bell.tar.gz'
-tar -zxvf bell.tar.gz
-cd bell
+git clone git://git.sv.gnu.org/liberty-eiffel.git
+cd liberty-eiffel
+# wget 'https://quantum-mirror.hu/mirrors/pub/gnusavannah/liberty-eiffel/bell.tar.gz'
+# tar -zxvf bell.tar.gz
+# cd bell
 ./install.sh -bootstrap
 PATH=$PATH:~/bell/target/bin
 export PATH
