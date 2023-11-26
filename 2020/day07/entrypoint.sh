7853980297c4abac7890aaec30dc35a501438fa7
Michał Grygierzec <michal.grygierzec@protonmail.com>
2023-11-26 18:49:16 +0100

    done

--- 2020/day07/entrypoint.sh
+++ 2020/day07/entrypoint.sh
@@ -4,8 +4,6 @@ echo 'deb [trusted=yes] http://apt.liberty-eiffel.org/ release main' >> /etc/apt
 apt-get --yes update
 apt-get --yes install liberty-eiffel-all
 
-echo $PWD
-ls -al
 cd 2020/day07
 se compile day07.e -o day07 -boost -O2
 ./day07
