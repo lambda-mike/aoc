69ffc6b3b4250b66f3ada7f4f4424021e48abc6a
Michał Grygierzec <michal.grygierzec@protonmail.com>
2023-11-26 14:04:58 +0100

    apt

--- 2020/day07/install.sh
+++ 2020/day07/install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+apt-get update
+apt-get install git build-essential castxml libgc-dev
 wget 'https://quantum-mirror.hu/mirrors/pub/gnusavannah/liberty-eiffel/bell.tar.gz'
 tar -zxvf bell.tar.gz
 cd bell
